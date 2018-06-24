4187610f9f015697fdf1b4bc51afa666b413322e
armand <armand.foucault@telecom-bretagne.eu>
2018-06-24 15:23:01 +0200

    Initial commit

---
+++ lib/words_tree.ex
@@ -0,0 +1,18 @@
+defmodule WordsTree do
+  @moduledoc """
+  Documentation for WordsTree.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WordsTree.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/words_tree_test.exs
@@ -0,0 +1,8 @@
+defmodule WordsTreeTest do
+  use ExUnit.Case
+  doctest WordsTree
+
+  test "greets the world" do
+    assert WordsTree.hello() == :world
+  end
+end
