f574aa5aa02d17226b47a960ea3247a1cde00497
armand <armand.foucault@telecom-bretagne.eu>
2018-06-24 16:54:36 +0200

    Wrote insertion

--- lib/words_tree.ex
+++ lib/words_tree.ex
@@ -1,18 +1,58 @@
 defmodule WordsTree do
-  @moduledoc """
-  Documentation for WordsTree.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  def start_link(opts) do
+    GenServer.start_link(WordsTree.Server, :ok, opts)
+  end
 
-  ## Examples
+  def insert(tree, word) do
+  end
 
-      iex> WordsTree.hello
-      :world
+  def lookup(tree, prefix) do
+  end
+end
 
-  """
-  def hello do
-    :world
+
+defmodule WordsTree.Server do
+  def init(:ok) do
+    {:ok, {}}
+  end
+
+  def handle_call({:insert, word}, _from, tree) do
+  end
+
+  def handle_call(_), do: :not_implemented
+  def handle_cast(_), do: :not_implemented
+
+  def insert(roots, word)
+
+  def insert([], <<char>>) do
+    [{char, [], true}]
+  end
+
+  def insert([], <<char>> <> word) do
+    [{
+      char, 
+      insert([], word), 
+      false}]
+  end
+
+  def insert([{char, children, _is_a_word} | rest], <<char>>) do
+    [{char, children, true} | rest]
+  end
+
+  def insert([{char, children, is_a_word} | rest], <<char>> <> word) do
+    [
+      {
+        char, 
+        insert(children, word), 
+        is_a_word
+      } 
+      | rest
+    ]
+  end
+
+  def insert([tree | rest], word) do
+    [tree | insert(rest, word)]
   end
 end
