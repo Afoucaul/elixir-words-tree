3c42491bd1ab7a0dca238cee9dafb9e28921040b
Armand <armand.foucault@kbrwadventure.com>
2018-06-25 11:34:55 +0200

    Updated genserver tests

--- test/genserver_test.exs
+++ test/genserver_test.exs
@@ -6,18 +6,24 @@ defmodule WordsTree.GenServerTest do
 
   setup do
     server = WordsTree.create
+    words = ["a", "b", "ab"]
+    tree = [{?a, [{?b, [], true}], true}, {?b, [], true}]
     %{
-      server: server
+      server: server,
+      words: words,
+      tree: tree
     }
   end
 
-  test "tree insertion", %{server: server} do
-    words = ["a", "b", "ab"]
-
+  test "tree insertion", %{server: server, words: words, tree: tree} do
     words
     |> Enum.map(&(WordsTree.insert(server, &1)))
 
-    WordsTree.get_tree(server)
-    |> IO.inspect
+    assert WordsTree.get_tree(server) == tree
+  end
+
+  test "blocking insertion of a words list", %{server: server, words: words, tree: tree} do
+    WordsTree.blocking_insert(server, words)
+    assert WordsTree.get_tree(server) == tree
   end
 end
